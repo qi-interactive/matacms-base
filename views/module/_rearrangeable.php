d8aee9a06df43f6cd32b41ab534df04530fa0a85
Michal Fiedorowicz <michal@qi-interactive.com>
2015-04-22 14:13:29 +0100

    Update

--- views/module/_rearrangeable.php
+++ views/module/_rearrangeable.php
@@ -97,14 +97,14 @@ $('.smooth-sortable li').draggable(
             var tickIcon = $('.tick-icon', ui.helper);
             var rearrangeableIcon = $('.rearrangeable-icon', ui.helper);
             
-            tickIcon.fadeOut(100);
-            rearrangeableIcon.fadeOut(100);
+            tickIcon.fadeOut(250);
+            rearrangeableIcon.fadeOut(250);
 
 
             tickIcon.fadeIn(100, function() {
                 setTimeout(function() {
-                    tickIcon.fadeOut(100);
-                    rearrangeableIcon.fadeIn(100);
+                    tickIcon.fadeOut(250);
+                    rearrangeableIcon.fadeIn(250);
                 }, 2500);
             });
             $('.smooth-sortable li').draggable('enable');
