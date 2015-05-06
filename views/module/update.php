3975d410442dfdafc663af2f5a1e73ed28f9a454
Marcin Wiatr <marcin@qi-interactive.com>
2015-05-06 15:04:18 +0100

    Updated flow

--- views/module/update.php
+++ views/module/update.php
@@ -2,18 +2,18 @@
 
 use yii\helpers\Html;
 use mata\arhistory\behaviors\HistoryBehavior;
-
+use matacms\theme\simple\assets\ModuleUpdateAsset;
 $this->title = 'Update ' . $model->getModelLabel() . ': ' . ' ' . $model->getLabel();
-$this->params['breadcrumbs'][] = ['label' => 'Content Blocks', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->getLabel(), 'url' => ['view', 'id' => $model->getPrimaryKey()]];
-$this->params['breadcrumbs'][] = 'Update';
+
+ModuleUpdateAsset::register($this);
+
 ?>
 
-<div><?= Html::a("Back to list view", sprintf("/mata-cms/%s/%s", $this->context->module->id, $this->context->id), ['id' => 'back-to-list-view']);?></div>
+<div><?php // echo Html::a("Back to list view", sprintf("/mata-cms/%s/%s", $this->context->module->id, $this->context->id), ['id' => 'back-to-list-view']);?></div>
 
 <?php
 
-echo Html::a("Versions", sprintf("history?documentId=%s&returnURI=%s", $model->getDocumentId()->getId(), Yii::$app->request->url), ['id' => 'versions-link']);
+// echo Html::a("Versions", sprintf("history?documentId=%s&returnURI=%s", $model->getDocumentId()->getId(), Yii::$app->request->url), ['id' => 'versions-link']);
 
 ?>
 <div class="content-block-update">
@@ -25,3 +25,12 @@ echo Html::a("Versions", sprintf("history?documentId=%s&returnURI=%s", $model->g
     ]) ?>
 
 </div>
+
+<script>
+
+	parent.mata.simpleTheme.header
+	.setBackToListViewURL("<?= sprintf("/mata-cms/%s/%s", $this->context->module->id, $this->context->id) ?>")
+	.setVersionsURL('<?= sprintf("/mata-cms/%s/%s/history?documentId=%s&returnURI=%s", $this->context->module->id, $this->context->id, urlencode($model->getDocumentId()->getId()), Yii::$app->request->url) ?>')
+	.show();
+
+</script>
