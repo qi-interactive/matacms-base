afa34992226b4164441a51db98c8db8e457664b9
Michal Fiedorowicz <michal@qi-interactive.com>
2015-05-06 18:16:59 +0100

    Update

--- views/module/index.php
+++ views/module/index.php
@@ -131,7 +131,7 @@ $.pjax.reload({container:"#w0", "url" : "?" + decodeURIComponent($.param(reqAttr
 <script>
 
     parent.mata.simpleTheme.header
-    .setText('YOU\'RE IN <?= $this->context->module->id ?> MODULE')
+    .setText('YOU\'RE IN <?= Inflector::camel2words($this->context->module->id) ?> MODULE')
     .hideBackToListView()
     .hideVersions()
     .show();
