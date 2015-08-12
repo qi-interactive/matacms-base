fb9a1667f2748037d22eae0dafdd1456bec56276
Michal Fiedorowicz <michal@qi-interactive.com>
2015-08-12 09:55:37 +0100

    Added support for multiple notifications with same key

--- filters/NotificationFilter.php
+++ filters/NotificationFilter.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
  * @link http://www.matacms.com/
  * @copyright Copyright (c) 2015 Qi Interactive Limited
@@ -22,19 +22,32 @@ class NotificationFilter extends Behavior {
 	}
 
 	public function beforeAction($event) {
-		
+
 		$this->observe($event, Controller::EVENT_MODEL_CREATED, "%s <strong>%s</strong> has been <strong>created</strong>.");
 		$this->observe($event, Controller::EVENT_MODEL_UPDATED, "%s <strong>%s</strong> has been <strong>updated</strong>.");
 		$this->observe($event, Controller::EVENT_MODEL_DELETED, "%s <strong>%s</strong> has been <strong>deleted</strong>.");
 
 		Event::on(View::className(), View::EVENT_BEGIN_BODY, function ($event) {
 			foreach(\Yii::$app->session->getAllFlashes(true) as $key => $message) {
-				echo $event->sender->context->renderPartial("@matacms/views/site/_notification", [
-					"message" => $message,
-					"level" => $key
-					]);
+				if(is_array($message)) {
+					foreach($message as $el) {
+						echo $this->renderNotification($event, $key, $el);
+					}
+				}
+				else {
+					echo $this->renderNotification($event, $key, $message);
+				}
+
 			}
 		});
+	}
+
+	private function renderNotification($event, $key, $message)
+	{
+		return $event->sender->context->renderPartial("@matacms/views/site/_notification", [
+			"message" => $message,
+			"level" => $key
+			]);
 	}
 
 	private function observe($actionEvent, $eventToObserve, $messageFormat) {
@@ -42,16 +55,16 @@ class NotificationFilter extends Behavior {
 
 			$message = $event->getMessage();
 
-			if (!is_string($message) && $event->name == Controller::EVENT_MODEL_DELETED && 
+			if (!is_string($message) && $event->name == Controller::EVENT_MODEL_DELETED &&
 				$message->hasErrors()) {
 				$event->setLevel(MessageEvent::LEVEL_WARNING);
 				$event->data = $message->getTopError();
 			}
 
 			if ($message instanceof \matacms\interfaces\HumanInterface) {
-				\Yii::$app->getSession()->setFlash($event->getLevel(), sprintf($event->data, $message->getModelLabel(), $message->getLabel()));
+				\Yii::$app->getSession()->addFlash($event->getLevel(), sprintf($event->data, $message->getModelLabel(), $message->getLabel()));
 			} else if(is_string($message)) {
-				\Yii::$app->getSession()->setFlash($event->getLevel(), $message);
+				\Yii::$app->getSession()->addFlash($event->getLevel(), $message);
 			} else {
 				throw new \yii\web\ServerErrorHttpException(sprintf("Cannot handle %s", get_class($message)));
 			}
